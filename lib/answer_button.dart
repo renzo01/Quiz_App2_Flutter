75d9c2976a75fd87ff6f2704bdab4648493033a8
Renzo Gomez <renzogomez577@gmail.com>
2026-03-15 17:47:08 -0500

    Add AnswerButton widget and update QuestionsScreen to display questions and answers

---
+++ lib/answer_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({super.key, required this.answerText, required this.onTap,});
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding:  EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        foregroundColor: Colors.white,
+        backgroundColor: Color.fromARGB(255, 96, 35, 153),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
+import 'package:adv_basics/answer_button.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,15 +13,16 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('The questions will go here!'),
-          ElevatedButton(onPressed: () {}, child: Text('Answer 1')),
-          ElevatedButton(onPressed: () {}, child: Text('Answer 2')),
-          ElevatedButton(onPressed: () {}, child: Text('Answer 3')),
+          Text(currentQuestion.text, style: TextStyle(color: Colors.white),),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {},);
+          }),
         ],
       ),
     );
